82367fb000d1b4e43d2398e15936992d6f1ef845
Martin Shanahan <m.shanahan914@gmail.com>
2024-08-21 20:58:23 +0100

    refactoring routes.py code

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
+import secrets
+import string
 from flask import Blueprint, jsonify, request
 from .models import Url
 from .extensions import db
-import secrets
-import string
+
 
 main = Blueprint('main', __name__)
 
@@ -15,47 +16,64 @@ def generate_short(length=6):
 def generate_unique_short():
     while True:
         candidate = generate_short()
-        if not db.session.query(Url).filter_by(shortCode=generate_short()).first():
+        if not db.session.query(Url).filter_by(shortCode=candidate).first():
             return candidate 
+        
+def get_url_item(short_code):
+    return Url.query.filter(Url.shortCode == short_code).first_or_404()
+        
+
+def url_to_json(item):
+    return {
+        "id": item.id,
+        "url": item.url,
+        "shortCode": item.shortCode,
+        "createdAt": item.created,
+        "updated": item.updated
+    }
 
 
-# post -> create new shortCode
 @main.route("/shorten", methods=["POST"])
 def create_code():
-    data = request.json     
-    short_url = generate_unique_short()
-    new_url = Url(url=data['url'], shortCode = short_url)
+    data = request.json  
+    if 'url' not in data:
+        return jsonify ({"message": "you must provide a 'url' field"}), 400   
+    
+    short_code = generate_unique_short()
+    new_url = Url(url=data['url'], shortCode = short_code)
     db.session.add(new_url)
     db.session.commit()
-    item = Url.query.filter(Url.shortCode == short_url).first_or_404()
-    return jsonify({"id":item.id,"url": item.url, "shortCode": item.shortCode, "createdAt": item.created, "updated": item.updated}), 201
+    item = get_url_item(short_code)
+    return jsonify(url_to_json(item)), 201
 
 
-@main.route("/shorten/<short_url>", methods=["GET"])
-def get_url(short_url):
-    item = Url.query.filter(Url.shortCode == short_url).first_or_404()
-    return jsonify({"id":item.id,"url": item.url, "shortCode": item.shortCode, "createdAt": item.created, "updated": item.updated}), 200
+@main.route("/shorten/<short_code>", methods=["GET"])
+def get_url(short_code):
+    item = get_url_item(short_code)
+    item.accessCount = item.accessCount + 1
+    db.session.commit()
+    return jsonify(url_to_json(item)), 200
 
 
-@main.route("/shorten/<short_url>", methods=["PUT"])
-def update_short(short_url):
+@main.route("/shorten/<short_code>", methods=["PUT"])
+def update_short(short_code):
     data = request.json
-    item = Url.query.filter(Url.shortCode == short_url).first_or_404()
+    item = get_url_item(short_code)
     item.url = data.get('url', item.url)
     db.session.commit()
-    item = Url.query.filter(Url.shortCode == short_url).first_or_404()
-    return jsonify({"id":item.id,"url": item.url, "shortCode": item.shortCode, "createdAt": item.created, "updated": item.updated}), 200
+    item = get_url_item(short_code)
+    return jsonify(url_to_json(item)), 200
     
 
-@main.route("/shorten/<short_url>", methods=["DELETE"])
-def delete_short(short_url):
-    item = Url.query.filter(Url.shortCode == short_url).first_or_404()
+@main.route("/shorten/<short_code>", methods=["DELETE"])
+def delete_short(short_code):
+    item = get_url_item(short_code)
     db.session.delete(item)
     db.session.commit()
-    return 204   
+    return '', 204   
 
 
-@main.get("/shorten/<short_url>/stats")
-def stats(short_url):
-    item = Url.query.filter(Url.shortCode == short_url).first_or_404()
-    return jsonify({"id":item.id,"url": item.url, "shortCode": item.shortCode, "createdAt": item.created, "updated": item.updated, "accessCount": item.accessCount}), 200
+@main.get("/shorten/<short_code>/stats")
+def stats(short_code):
+    item = get_url_item(short_code)
+    return jsonify(url_to_json(item) |{"accessCount" : item.accessCount}), 200
